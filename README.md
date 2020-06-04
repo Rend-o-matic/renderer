ad07b75f46976e411f4d62abf62c7d032f965121
Glynn Bird <glynn.bird@gmail.com>
2020-06-04 13:22:22 +0100

    first stab at renderer

---
+++ README.md
@@ -0,0 +1,65 @@
+# renderer
+
+> They call me the renderer.....the renderer.....
+
+A serverless function that takes a `choirId` & `songId`:
+
+- gets a list of song parts from the Choirless API.
+- pulls down the video files from COS for each video to local temporary storage
+- stitches the videos into a video wall using Stitcheroo, creating a new local file
+- uploads the finished video to COS
+
+## Parameters
+
+The `main` function expects an object with the following attributes:
+
+- `choirId` - the id choir whose song is to be rendered.
+- `songId` - the id of the song to render.
+- `COS_API_KEY` - the 'apikey' from the service credentials.
+- `COS_INSTANCE_ID` - the 'resource_instance_id' from the service credentials.
+- `COS_ENDPOINT` - the [S3 API endpoint](https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-endpoints) not to be confused with 'endpoints' from the service credentials e.g. 's3.eu-gb.cloud-object-storage.appdomain.cloud'.
+- `COS_BUCKET` - the name of the bucket to read and write videos.
+- `COUCH_URL` - the URL of the Cloudant service (including credentials).
+- `COUCH_USERS_DATABASE` - the name of the users database e.g. 'choirless_users'.
+- `COUCH_CHOIRLESS_DATABASE` - the name of the main database e.g. 'choirless'.
+- `COUCH_KEYS_DATABASE` - the name of the keys database e.g. 'choirless_keys'.
+- `COUCH_QUEUE_DATABASE` - the name of the queue database e.g. 'choirless_queue'.
+
+## Example usage
+
+```js
+const go = async () => {
+  const opts = {
+    COS_API_KEY: 'apikey',
+    COS_ENDPOINT: 'some.domain.name',
+    COS_INSTANCE_ID: 'crn:some:id',
+    COS_BUCKET: 'choirless',
+    COUCH_URL: 'https://u:p@myhost.cloudant.com',
+    COUCH_USERS_DATABASE: 'choirless_users',
+    COUCH_CHOIRLESS_DATABASE: 'choirless',
+    COUCH_KEYS_DATABASE: 'choirless_keys',
+    COUCH_QUEUE_DATABASE: 'choirless_queue',
+    songId: 'songId',
+    choirId: 'choirId'
+  }
+  try {
+    await main(opts)
+  } catch (e) {
+    console.log('ERROR', e)
+  }
+}
+```
+
+## Assumptions
+
+Song part videos are stored in the COS bucket with keys
+
+```
+<choirId>/<songId>/<partId>.mp4
+```
+
+The final video is stored as
+
+```
+<choirId>/<songId>/final.mp4
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+const sticheroo = require('stitcheroo')
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const ibmCOS = require('ibm-cos-sdk')
+
+// read a COS object to a local file using streams
+const pullFromCOS = async (cos, params, localFilename) => {
+  console.log('pullFromCOS', params, '--->', localFilename)
+  return new Promise((resolve, reject) => {
+    const ws = fs.createWriteStream(localFilename)
+    cos.getObject(params).createReadStream().pipe(ws)
+      .on('close', resolve)
+      .on('error', reject)
+  })
+}
+
+const main = async (opts) => {
+  // put incoming opts into environment variables for choirlessapi
+  const OPTS = ['COUCH_URL', 'COUCH_USERS_DATABASE', 'COUCH_CHOIRLESS_DATABASE', 'COUCH_KEYS_DATABASE', 'COUCH_QUEUE_DATABASE']
+  for (const i in OPTS) {
+    process.env[OPTS[i]] = opts[OPTS[i]]
+  }
+  const choirlessAPI = require('choirlessapi')
+
+  // get the songid/choirId parameters
+  const songId = opts.songId
+  const choirId = opts.choirId
+  if (!songId || !choirId) {
+    return { ok: false, message: 'missing parameterss' }
+  }
+
+  // configure COS
+  const config = {
+    endpoint: opts.COS_ENDPOINT,
+    apiKeyId: opts.COS_API_KEY,
+    serviceInstanceId: opts.COS_INSTANCE_ID
+  }
+  const cos = new ibmCOS.S3(config)
+
+  // get the song parts from the database
+  const response = await choirlessAPI.getChoirSongParts({ songId: songId, choirId: choirId })
+  console.log('choirlessAPI response', response)
+
+  // calculate the filenames of the videos
+  const videos = response.parts.map((p) => {
+    return path.join(p.choirId, p.songId, p.partId) + '.mp4'
+  })
+  console.log('COS keys', videos)
+
+  // fetch the song parts' mp4 files from COS
+  const tmp = os.tmpdir()
+  const localVideos = []
+  for (var i in videos) {
+    const localFilename = path.join(tmp, i + '.mp4')
+    await pullFromCOS(cos, { Bucket: opts.COS_BUCKET, Key: videos[i] }, localFilename)
+    localVideos.push(localFilename)
+  }
+  console.log('local video files', localVideos)
+
+  // render the videos to a single video
+  const filename = await sticheroo(localVideos, false)
+  console.log('finished video', filename)
+
+  // write back to COS
+  console.log('writing finished video to cos')
+  await cos.putObject({
+    Bucket: opts.COS_BUCKET,
+    Key: path.join(choirId, songId, 'final') + '.mp4',
+    Body: fs.createReadStream(filename)
+  }).promise()
+  console.log('done')
+
+  // clean up temp files
+  fs.unlinkSync(filename)
+  for (i in localVideos) {
+    fs.unlinkSync(localVideos[i])
+  }
+
+  return { ok: true }
+}
+
+module.exports = {
+  main
+}

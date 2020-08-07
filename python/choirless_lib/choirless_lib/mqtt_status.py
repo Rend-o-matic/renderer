3b70d25daf4c477ff4540acb2679d2ac42e1b246
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-08-07 16:01:13 +0100

    Fixed Path import

---
+++ python/choirless_lib/choirless_lib/mqtt_status.py
@@ -0,0 +1,69 @@
+## Signed URL support
+import json
+import os
+import time
+from pathlib import Path
+
+import paho.mqtt.publish as publish
+
+def mqtt_status(stage=None):
+    stage_ = stage
+    
+    def wrap(method):
+        
+        def wrapped_f(args):
+
+            if stage_ is None:
+                stage = os.environ.get('__OW_ACTION_NAME')
+                
+                if stage is not None:
+                    stage = stage.split('/')[-1]
+                else:
+                    stage = 'unknown'
+
+            notification = args.get('notification', {})
+            key = args.get('key', notification.get('object_name', ''))
+
+            key_parts = Path(key).stem.split('+')
+            choir_id, song_id = key_parts[:2]
+            
+            msg = {'choir_id': choir_id,
+                   'song_id': song_id,
+                   'stage': stage,
+            }
+            
+            if stage in ['convert_format', 'calculate_alignment', 'trim_clip']:
+                msg['part_id'] = key_parts[2]
+        
+            mqtt_server, mqtt_port = args['mqtt_broker'].split(':')
+
+            t1 = time.time()
+            msg['event'] = 'start'
+            msg['start'] = int(t1)
+
+            publish.single(
+                f"choirless/{choir_id}/{song_id}/renderer/{stage}",
+                json.dumps(msg),
+                hostname=mqtt_server,
+                port=int(mqtt_port)
+            )
+            
+            result = method(args)
+            t2 = time.time()
+
+            msg['event'] = 'end'
+            msg['end'] = int(t2)
+            msg['duration'] = int(t2-t1)
+            
+            publish.single(
+                f"choirless/{choir_id}/{song_id}/renderer/{stage}",
+                json.dumps(msg),
+                hostname=mqtt_server,
+                port=int(mqtt_port)
+            )
+            
+            return result
+
+        return wrapped_f
+    
+    return wrap

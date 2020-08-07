d175b59ceee88b236f9e3efe9d8b7d9df8b6f5cf
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-08-07 14:24:43 +0100

    Include dependancies in setup

---
+++ python/choirless_lib/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Choirless serverless utility functions',
+    'author': 'Matt Hamiltone',
+    'url': 'https://github.com/choirless/renderer',
+    'download_url': 'https://github.com/choirless/renderer',
+    'author_email': 'mh@quernus.co.uk',
+    'version': '0.1',
+    'install_requires': ['ibm_boto3', 'paho-mqtt', 'requests'],
+    'packages': ['choirless_lib'],
+    'scripts': [],
+    'name': 'choirless_lib'
+}
+
+setup(**config)
+

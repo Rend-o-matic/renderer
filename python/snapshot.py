786c172a17ef86058482d41fd416c86c747ed9f2
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-08-12 10:36:00 +0100

    refactor to use choirless_lib

--- python/snapshot.py
+++ python/snapshot.py
@@ -6,8 +6,7 @@ import numpy as np
 import tempfile
 from pathlib import Path
 
-import ibm_boto3
-from ibm_botocore.client import Config
+from choirless_lib import create_cos_client
 
 import ffmpeg
 
@@ -16,15 +15,16 @@ SAMPLE_RATE = 44100
 
 def main(args):
 
-    cos = createCOSClient(args)
+    cos = create_cos_client(args)
 
     if not cos:
         raise ValueError(f"could not create COS instance")
 
-    bucket = args.get('bucket')
-
     notification = args.get('notification', {})
-    key = notification.get('object_name', '')
+    key = args.get('key', notification.get('object_name', ''))
+
+    bucket = args['preview_bucket']
+
     if key.endswith(".jpg"):
         return {}
 
@@ -43,70 +43,7 @@ def main(args):
 
         cos.upload_file(str(new_path), bucket, str(new_path.name))
 
-        args["snapshot_key"] = str(new_path.name)
+        ret = {"status": "ok",
+               "snapshot_key": str(new_path.name)}
 
-        return args
-
-
-def createCOSClient(args):
-    """
-    Create a ibm_boto3.client using the connectivity information
-    contained in args.
-
-    :param args: action parameters
-    :type args: dict
-    :return: An ibm_boto3.client
-    :rtype: ibm_boto3.client
-    """
-
-    # if a Cloud Object Storage endpoint parameter was specified
-    # make sure the URL contains the https:// scheme or the COS
-    # client cannot connect
-    if args.get('endpoint') and not args['endpoint'].startswith('https://'):
-        args['endpoint'] = 'https://{}'.format(args['endpoint'])
-
-    # set the Cloud Object Storage endpoint
-    endpoint = args.get('endpoint',
-                        'https://s3.us.cloud-object-storage.appdomain.cloud')
-
-    # extract Cloud Object Storage service credentials
-    cos_creds = args.get('__bx_creds', {}).get('cloud-object-storage', {})
-
-    # set Cloud Object Storage API key
-    api_key_id = \
-        args.get('apikey',
-                 args.get('apiKeyId',
-                          cos_creds.get('apikey',
-                                        os.environ
-                                        .get('__OW_IAM_NAMESPACE_API_KEY')
-                                        or '')))
-
-    if not api_key_id:
-        # fatal error; it appears that no Cloud Object Storage instance
-        # was bound to the action's package
-        return None
-
-    # set Cloud Object Storage instance id
-    svc_instance_id = args.get('resource_instance_id',
-                               args.get('serviceInstanceId',
-                                        cos_creds.get('resource_instance_id',
-                                                      '')))
-    if not svc_instance_id:
-        # fatal error; it appears that no Cloud Object Storage instance
-        # was bound to the action's package
-        return None
-
-    ibm_auth_endpoint = args.get('ibmAuthEndpoint',
-                                 'https://iam.cloud.ibm.com/identity/token')
-
-    # Create a Cloud Object Storage client using the provided
-    # connectivity information
-    cos = ibm_boto3.client('s3',
-                           ibm_api_key_id=api_key_id,
-                           ibm_service_instance_id=svc_instance_id,
-                           ibm_auth_endpoint=ibm_auth_endpoint,
-                           config=Config(signature_version='oauth'),
-                           endpoint_url=endpoint)
-
-    # Return Cloud Object Storage client
-    return cos
+        return ret

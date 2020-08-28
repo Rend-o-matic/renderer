c1e87fb1403eafd33faa0a8ac9fe29ec2a4cfa28
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-08-28 18:14:48 +0100

    do initial probe first to explicily check if we have audio and/or video

--- python/convert_format.py
+++ python/convert_format.py
@@ -46,58 +46,73 @@ def main(args):
 
     kwargs = {}
 
+    ## Probe pass
+    # First probe the file to see if we have audio and/or video streams
+    try:
+        output = ffmpeg.probe(get_input_url(key))
+    except Exception as e:
+        print("ffprobe error", e.stderr)
+        return({'error': str(e)})
+
+    stream_types = set([ s['codec_type'] for s in output['streams'] ])
+    audio_present = 'audio' in stream_types
+    video_present = 'video' in stream_types
+
+    print("Audio present:" , audio_present)
+    print("Video present:", video_present)
+
     ## Two pass loudness normalisation
     # First pass, get details
+    if audio_present:
+        print("Doing first pass")
+        stream = ffmpeg.input(get_input_url(key),
+                              seekable=0)
+        audio = stream.audio
+        audio = audio.filter('silenceremove',
+                             stop_periods=-1,
+                             stop_duration=1,
+                             stop_threshold='-60dB')
+        audio = audio.filter('loudnorm',
+                             i=-14,
+                             dual_mono=True,
+                             print_format='json')
+        pipeline = ffmpeg.output(audio,
+                                 "-",
+                                 format='null')
 
-    print("Doing first pass")
-    stream = ffmpeg.input(get_input_url(key),
-                          seekable=0)
-    audio = stream.audio
-    audio = audio.filter('silenceremove',
-                         stop_periods=-1,
-                         stop_duration=1,
-                         stop_threshold='-60dB')
-    audio = audio.filter('loudnorm',
-                         i=-14,
-                         dual_mono=True,
-                         print_format='json')
-    pipeline = ffmpeg.output(audio,
-                             "-",
-                             format='null')
+        cmd = pipeline.compile()
+        print("ffmpeg command to run: ", cmd)
 
-    cmd = pipeline.compile()
-    print("ffmpeg command to run: ", cmd)
+        stdout, stderr = pipeline.run(capture_stdout=True,
+                                      capture_stderr=True)
+        output = stdout + stderr
+        output_lines = [line.strip() for line in output.decode().split('\n')]
+        loudnorm_start = False
+        loudnorm_end = False
 
-    stdout, stderr = pipeline.run(capture_stdout=True,
-                                  capture_stderr=True)
-    output = stdout + stderr
-    output_lines = [line.strip() for line in output.decode().split('\n')]
-    loudnorm_start = False
-    loudnorm_end = False
-    
-    for index, line in enumerate(output_lines):
-        if line.startswith('[Parsed_loudnorm'):
-            loudnorm_start = index + 1
-            continue
-        if loudnorm_start and line.startswith('}'):
-            loudnorm_end = index + 1
-            break
+        for index, line in enumerate(output_lines):
+            if line.startswith('[Parsed_loudnorm'):
+                loudnorm_start = index + 1
+                continue
+            if loudnorm_start and line.startswith('}'):
+                loudnorm_end = index + 1
+                break
 
-    if not (loudnorm_start and loudnorm_end):
-        raise Exception("Could not parse loudnorm stats; no loudnorm-related output found")
+        if not (loudnorm_start and loudnorm_end):
+            raise Exception("Could not parse loudnorm stats; no loudnorm-related output found")
 
-    try:
-        loudnorm_stats = json.loads('\n'.join(output_lines[loudnorm_start:loudnorm_end]))
-    except Exception as e:
-        raise Exception("Could not parse loudnorm stats; wrong JSON format in string: {e}")
+        try:
+            loudnorm_stats = json.loads('\n'.join(output_lines[loudnorm_start:loudnorm_end]))
+        except Exception as e:
+            raise Exception("Could not parse loudnorm stats; wrong JSON format in string: {e}")
 
-    print("json stats", loudnorm_stats)
+        print("json stats", loudnorm_stats)
 
-    target_offset = float(loudnorm_stats['target_offset'])
-    input_i=float(loudnorm_stats['input_i'])
-    input_lra=float(loudnorm_stats['input_lra'])
-    input_tp=float(loudnorm_stats['input_tp'])
-    input_thresh=float(loudnorm_stats['input_thresh'])
+        target_offset = float(loudnorm_stats['target_offset'])
+        input_i=float(loudnorm_stats['input_i'])
+        input_lra=float(loudnorm_stats['input_lra'])
+        input_tp=float(loudnorm_stats['input_tp'])
+        input_thresh=float(loudnorm_stats['input_thresh'])
 
     # Second pass, apply normalisation
     print("Doing second pass")
@@ -107,23 +122,27 @@ def main(args):
     video = video.filter('scale', 640, 480,
                          force_original_aspect_ratio='decrease',
                          force_divisible_by=2)
-    audio = stream.audio
+    if audio_present:
+        audio = stream.audio
 
-    # If the normalisation appears to detect no sound then just mute audio
-    if input_i == -math.inf or input_lra == 0 or target_offset == math.inf:
-        audio = audio.filter('volume', 0)
+        # If the normalisation appears to detect no sound then just mute audio
+        if input_i == -math.inf or input_lra == 0 or target_offset == math.inf:
+            audio = audio.filter('volume', 0)
+        else:
+            audio = audio.filter('loudnorm',
+                                 i=-14,
+                                 offset=target_offset,
+                                 measured_i=input_i,
+                                 measured_lra=input_lra,
+                                 measured_tp=input_tp,
+                                 measured_thresh=input_thresh,
+                                 linear=True,
+                                 dual_mono=True,
+                                 print_format='summary')
+            audio = audio.filter('aresample', 44100)
     else:
-        audio = audio.filter('loudnorm',
-                             i=-14,
-                             offset=target_offset,
-                             measured_i=input_i,
-                             measured_lra=input_lra,
-                             measured_tp=input_tp,
-                             measured_thresh=input_thresh,
-                             linear=True,
-                             dual_mono=True,
-                             print_format='summary')
-        audio = audio.filter('aresample', 44100)
+        audio = ffmpeg.input('anullsrc',
+                             format='lavfi').audio
 
     pipeline = ffmpeg.output(audio,
                              video,
@@ -133,6 +152,7 @@ def main(args):
                              vcodec='libx264',
                              method='PUT',
                              preset='slow',
+                             shortest=None,
                              seekable=0,
                              r=25,
                              ac=1,

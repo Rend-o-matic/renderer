96926e1ad056d47b985731d34c473fb08bd0c773
Glynn Bird <glynn.bird@gmail.com>
2020-09-08 16:02:34 +0100

    first attempt at creating an action which updates the render status

---
+++ python/renderer_status.py
@@ -0,0 +1,32 @@
+import requests
+from urllib.parse import urljoin
+
+def main(args):
+
+    # Tell the API the current render sttus
+    try:
+        # get passed-in arguments
+        api_url = args['CHOIRLESS_API_URL']
+        api_key = args['CHOIRLESS_API_KEY']
+        choir_id = args['choir_id']
+        song_id = args['song_id']
+        part_id = args['part_id']
+        status = args['status']
+
+        # pass the apikey in the URL
+        params = { 'apikey': api_key }
+
+        # pass everything else in the POST body
+        payload = {
+                    'choirId': choir_id,
+                    'songId': song_id,
+                    'partId': part_id,
+                    'status': status
+                  }
+
+        # make HTTP POST request with application/json header
+        req = requests.post(urljoin(api_url, 'render'), params=params, json=payload)
+        req.json()
+
+    except:
+        print("Could not post render status into the API: choirId {choir_id} songId {song_id} partId {part_id} status {status}")

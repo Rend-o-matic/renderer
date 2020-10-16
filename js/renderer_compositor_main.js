6f55becbad7e145e5040ba8929607bcc000de008
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-10-16 12:24:57 +0100

    Backwards compatible start that wraps and old-style input_spec in a single scene

--- js/renderer_compositor_main.js
+++ js/renderer_compositor_main.js
@@ -25,37 +25,53 @@ const main = async (opts) => {
     let definition_object = await cos.getObject({ Bucket: opts.definition_bucket, Key: key }).promise()
     let definition = JSON.parse(definition_object['Body'])
 
-    // Calculate number of rows
-    let rows = new Set()
-    let input_specs = definition.inputs
-    input_specs.forEach(spec => {
-	let [x, y] = spec.position || [-1, -1]
-	rows.add(y)
-    })
-    rows = Array.from(rows)
-    rows.sort((a,b) => parseInt(a) - parseInt(b))
+    // if we have scenes then loop per scene, if not add artificial scene in
+    let scenes = []
+    if (definition.scenes == undefined) {
+	scenes.push({"scene_id": 1,
+		     "inputs": definition.inputs
+		    })
+    } else {
+	scenes = definition.scenes
+    }
 
-    let num_rows = rows.length
-
-    // Calculate the hash of our rows
-    let rows_str = rows.join("-")
-    let rows_hash = crypto.createHash('sha1').update(rows_str).digest('hex').slice(0,8)
+    let actions = [] 
+    const ow = openwhisk()
     let run_id = uuidv4().slice(0,8)
+    
+    scenes.forEach(scene => {
+	// Get the inputs for this scene
+	input_specs = scene.inputs
+	// Calculate number of rows
+	let rows = new Set()
+	input_specs.forEach(spec => {
+	    let [x, y] = spec.position || [-1, -1]
+	    rows.add(y)
+	})
+	rows = Array.from(rows)
+	rows.sort((a,b) => parseInt(a) - parseInt(b))
 
-    // Invoke all the child actions
-    const ow = openwhisk();
-    let actions = rows.map(row => {
-	let params = {"row_num": row,
-		      "run_id": run_id,
-		      "rows_hash": rows_hash,
-		      "compositor": "combined",
-		      "key": key,
-		      "definition_key": key}
-	return ow.actions.invoke({name: "choirless/renderer_compositor_child",
-				  params: params,
-				  blocking: false})
+	let num_rows = rows.length
+
+	// Calculate the hash of our rows
+	let rows_str = rows.join("-")
+	let rows_hash = crypto.createHash('sha1').update(rows_str).digest('hex').slice(0,8)
+
+	// Invoke all the child actions
+	rows.forEach(row => {
+	    let params = {"row_num": row,
+			  "run_id": run_id,
+			  "rows_hash": rows_hash,
+			  "compositor": "combined",
+			  "key": key,
+			  "definition_key": key}
+	    let action =  ow.actions.invoke({name: "choirless/renderer_compositor_child",
+					     params: params,
+					     blocking: false})
+	    actions.push(action)
+	})
     })
-
+    
     // Await for the child calls to all return with their activation ID
     let res = await Promise.all(actions)
     let activation_ids = res.map(r => {	return r.activationId  })
